f615a19628b852a64b2eb421f2b246a3930cf786
Samuel Medina <same.isro16@gmail.com>
2023-11-07 00:03:50 -0600

    Added connection to OpenAI API. Chatbot fully functional. Styles pending

--- src/js/main.js
+++ src/js/main.js
@@ -1,43 +1,73 @@
-// const Configuration = require("openai").Configuration;
-// const OpenAIApi = require("openai").OpenAIApi;
+const chatInput = document.querySelector(".chat-input textarea");
+const sendChatBtn = document.querySelector(".chat-input span");
+const chatbox= document.querySelector(".chatbox");
 
-// const configuration = new Configuration({
-//     apiKey: "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",
-//     organization: "org-YpCC4LWerl62kAV01h6bx9bY"
-// });
+let userMessage;
+const API_KEY = "";
+const inputInitHeight = chatInput.scrollHeight;
 
-// const openai = new OpenAIApi(configuration);
-
-// async function main(){
-//     const response = await openai.createCompletion({ // Corregido a createCompletion
-//         model: "text-davinci-003",
-//         prompt: "Cuenta un chiste"
-//     });
-
-//     console.log(response);
-// }
-
-// main();
-
-//prueba conexión
-
-
-const { Configuration, OpenAIApi } = require("openai");
-
-const configuration = new Configuration({
-    apiKey: "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",
-    organization: "org-YpCC4LWerl62kAV01h6bx9bY"
-});
-
-const openai = new OpenAIApi(configuration);
-
-async function main() {
-    const response = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: "Cuenta un chiste"
-    });
-
-    console.log(response);
+const createChatLi = (message, className) => {
+    //Crear el li con los mensajes del usuario
+    const chatLi = document.createElement("li");
+    chatLi.classList.add("chat", className);
+    let chatContent = className === "outgoing" ? `<p></p>` : `<span class="material-symbols-outlined">smart_toy</span><p></p>`;
+    chatLi.innerHTML = chatContent;
+    chatLi.querySelector("p").textContent = message
+    return chatLi;
 }
 
-main();
+const generateResponse = (incomingChatLi) => {
+    const API_URL = "https://api.openai.com/v1/chat/completions";
+    const messageElement = incomingChatLi.querySelector("p");
+    const requestOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${API_KEY}`
+        },
+        body: JSON.stringify({
+            model: "gpt-3.5-turbo",
+            messages: [{role: "user", 
+            content: userMessage}]
+        })
+    }
+
+    //Hace el request por medio de POST a la API
+    fetch(API_URL, requestOptions).then(res => res.json()).then(data => {
+        messageElement.textContent = data.choices[0].message.content;
+    }).catch((error) => {
+        messageElement.textContent = "Algo salio mal. Por favor intenta de nuevo";
+    }).finally(() => chatbox.scrollTo(0, chatbox.scrollHeight));
+}
+
+const handleChat = () => {
+    userMessage = chatInput.value.trim();
+    if(!userMessage) return;
+    chatInput.value = "";
+    chatInput.computedStyleMap.height = `${inputInitHeight}px`;
+
+    //Agreaga el mensaje al chatbox
+    chatbox.appendChild(createChatLi(userMessage, "outgoing"));
+    chatbox.scrollTo(0, chatbox.scrollHeight);
+
+    setTimeout(() => {
+        const incomingChatLi = createChatLi("Un momento...", "incoming");
+        chatbox.appendChild(incomingChatLi);
+        chatbox.scrollTo(0, chatbox.scrollHeight);
+        generateResponse(incomingChatLi);
+    }, 600);
+}
+
+chatInput.addEventListener("input", () => {
+    chatInput.computedStyleMap.height = `${inputInitHeight}px`;
+    chatInput.computedStyleMap.height = `${chatInput.scrollHeight}px`;
+})
+
+chatInput.addEventListener("keydown", (e) => {
+    if(e.key === "Enter" && !e.shiftKey && window.innerWidth > 800){
+        e.preventDefault();
+        handleChat();
+    }
+})
+
+sendChatBtn.addEventListener("click", handleChat);
